0529c578c635e42e22c4c0766b7c5423344823bd
magma <tanayachyute@gmail.com>
2024-03-30 12:31:46 +0530

    added header and logo to section 3

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Section3 = () => {
-  return <div>Section3</div>;
+  return <div className="w-full h-[100vh] bg-[#FFF8EC]">
+
+    <div className="flex justify-end mr-10">
+      <img src={"/four_dot.png"} alt="" />
+    </div>
+    <div>
+
+     <div className="font-lora italic text-3xl lg:text-6xl ml-5 lg:ml-16 mt-28 font-bold text-[#502E23] flex justify-end mr-[100px]" >Behind the Shades: Crafting Excellence</div> 
+
+
+    </div>
+
+
+
+  </div>;
 };
 
 export default Section3;
