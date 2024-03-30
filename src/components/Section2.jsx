a632f9218ac29847773fcb9d81e8499c163265bb
magma <tanayachyute@gmail.com>
2024-03-30 12:14:34 +0530

    added section 2

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./section-2.css";
 
 const Section2 = () => {
-  return <div>Section2</div>;
+  return <div className="sec2">
+
+    <div className="discover">
+      <div>Discover the Shades 360 story</div>
+      <div className="dots"><img src={"/four_dot.png"} alt="" /></div>
+      
+    </div>
+
+    <div className="subtext">Born in LA, Shades 360 is the fusion of fashion and function. Our mission was to craft sunglasses that elevate your style and protect your eyes.
+    <br/>
+    <span>From city streets to sandy shores, we've got you covered.</span>
+
+    </div>
+    
+
+
+  </div>;
 };
 
 export default Section2;
